43c9ea563ae847b7de54e950390bbbff033946e3
Hugo Freitas <hugofreitas12@gmail.com>
2021-10-13 23:25:16 +0100

    ADD: Flowchart and prototypes on api.h

--- Assignments/1/report/listing/api.h
+++ Assignments/1/report/listing/api.h
@@ -5,3 +5,13 @@
  * @return 0 if success, and the error code otherwise
  */
 int i2c_read(char addr, char *data);
+/**
+ * @brief Push/pop to the buffer a specified command
+ * @param fifo: buffer
+ * @param data: byte to push/pop/send
+ * @return 0 if success, and the error code otherwise
+ */
+int fifo_push(fifo *f, char data);
+int fifo_pop(fifo *f, char *data);
+int fifo_init(fifo *fif);
+int ir_send(char data);
